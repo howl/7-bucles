59786b6a7961a99d205d7af003ceecf06fe2f6f1
David Santamaría Rogado <howl.nsp@gmail.com>
2026-04-22 14:28:11 +0200

    main.js: add exercise 2 loop
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ let acumulado = 0;
 for (let i = 1; i <= 100; i++)
   acumulado += i;
 console.log(acumulado);
+
+/* Números pares del 1 al 50 */
+for (let i = 1; i <= 50; i++)
+  if (!(i % 2))
+    console.log(i);
